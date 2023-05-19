bbfc25ab50923c9bf2c8cb7a3288946d9168b6b8
Ricardo Valdes <ricardovaldes@everfall-games.com>
2023-05-19 18:51:28 -0400

    Main Menu completed

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -5,16 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField] public enum MenuStates { main, game }
+    [SerializeField] private MenuStates CurrentState;
+
+    private GameObject StartMenuParent;
+    private GameObject SettingsMenuParent;
+
+    private Animator mainMenuAnimator;
 
     void Start()
     {
-        
+        DontDestroyOnLoad(this.gameObject);
+
+        CurrentState = MenuStates.main;
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        if (mainMenuAnimator == null) { mainMenuAnimator = this.GetComponent<Animator>(); }
+        if (StartMenuParent == null) { StartMenuParent = GameObject.FindGameObjectWithTag("StartSubMenu"); }
+        if (SettingsMenuParent == null) { SettingsMenuParent = GameObject.FindGameObjectWithTag("SettingsSubMenu"); }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        switch (CurrentState)
+        {
+            case MenuStates.main:
+                StartMenuParent.SetActive(true);
+                break;
+
+            case MenuStates.game:
+                StartMenuParent.SetActive(false);
+                SettingsMenu(2);
+                break;
+        }
     }
 
     public void LoadGameScene()
@@ -22,9 +45,33 @@ public class MainMenuManager : MonoBehaviour
         SceneManager.LoadScene("Game");
     }
 
-    public void SettingsMenu(bool state)
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
+        if (SceneManager.GetActiveScene().name != "MainMenu")
+        { 
+            mainMenuAnimator.Play("FadeIn");
+            CurrentState = MenuStates.game;
+            
+        }
+    }
 
+    public void StartGame()
+    {
+        mainMenuAnimator.Play("FadeOut");
+    }
+
+    public void SettingsMenu(int state) //STATES: 0 = main menu, 1 = in game 2 = closed
+    {
+        switch (state)
+        {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+
+        }
     }
 
     public void ExitGame()
